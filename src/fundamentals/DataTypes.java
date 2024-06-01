6c6b85b1544dbb56d48e8c37da6ed7d69872dead
sahil <sahiillx02@gmail.com>
2024-06-01 10:00:38 +0530

    fundamentals

---
+++ src/fundamentals/DataTypes.java
@@ -0,0 +1,29 @@
+package fundamentals;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        /*
+         * PRIMITIVE DATATYPES
+         *
+         * byte
+         * short
+         * int
+         * long
+         * float
+         * double
+         * boolean
+         * char
+         */
+
+        /*
+         * NON - PRIMITIVE DATATYPES
+         *
+         * String
+         * Arrays
+         * Classes
+         * Interfaces
+         */
+
+
+    }
+}
---
+++ src/fundamentals/Variables.java
@@ -0,0 +1,14 @@
+package fundamentals;
+
+public class Variables  {
+    public static void main(String[] args) {
+        // data_type variable_name;
+        // variable_name = value;
+
+        int count;
+        count = 5;
+
+        String name;
+        name = "Java";
+    }
+}
