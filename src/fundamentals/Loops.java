40c803873e7760e226a9e0ef91848e9c6b20e3b5
sahil <sahiillx02@gmail.com>
2024-06-02 11:05:25 +0530

    loops

---
+++ src/fundamentals/Loops.java
@@ -0,0 +1,32 @@
+package fundamentals;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        // FOR LOOP
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Iteration: " + i);
+//        }
+
+        // WHILE LOOP
+//        int i = 0;
+//        while (i < 5) {
+//            System.out.println("Iteration: " + i);
+//            i++;
+//        }
+
+        // DO WHILE LOOP
+//        int i = 0;
+//        do {
+//            System.out.println("Iteration: " + i);
+//            i++;
+//        } while (i < 5);
+
+        // FOR EACH LOOP
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        for (int i : numbers) {
+//            System.out.println("Number: " + i);
+//        }
+
+    }
+}
