3971026c7bdf8448279b26245edb55411148941d
sahil <sahiillx02@gmail.com>
2024-06-02 09:17:38 +0530

    operators

---
+++ src/fundamentals/Operators.java
@@ -0,0 +1,72 @@
+package fundamentals;
+
+public class Operators {
+    public static void main(String[] args) {
+        // ARITHMETIC OPERATORS
+//        int a = 10, b = 5;
+//        System.out.println("a + b = " + (a + b)); // 15
+//        System.out.println("a - b = " + (a - b)); // 5
+//        System.out.println("a * b = " + (a * b)); // 50
+//        System.out.println("a / b = " + (a / b)); // 2
+//        System.out.println("a % b = " + (a % b)); // 0
+
+        // RELATIONAL OPERATORS
+//        System.out.println("a == b: " + (a == b)); // false
+//        System.out.println("a != b: " + (a != b)); // true
+//        System.out.println("a > b: " + (a > b)); // true
+//        System.out.println("a < b: " + (a < b)); // false
+//        System.out.println("a >= b: " + (a >= b)); // true
+//        System.out.println("a <= b: " + (a <= b)); // false
+
+        // LOGICAL OPERATORS
+//        boolean x = true, y = false;
+//        System.out.println("x && y: " + (x && y)); // false
+//        System.out.println("x || y: " + (x || y)); // true
+//        System.out.println("!x: " + (!x)); // false
+//        System.out.println("!y: " + (!y)); // true
+
+        // ASSIGNMENT OPERATORS
+//        int n = 10;
+//        n += 5;
+//        System.out.println("n += 5 -> " + n); // 15
+//        n -= 3;
+//        System.out.println("n -= 3 -> " + n); // 12
+//        n *= 2;
+//        System.out.println("n *= 2 -> " + n); // 24
+//        n /= 4;
+//        System.out.println("n /= 4 -> " + n); // 6
+//        n %= 5;
+//        System.out.println("n %= 5 -> " + n); // 1
+
+        // BITWISE OPERATORS
+//        int a = 5, b = 3;
+//        System.out.println("a & b: " + (a & b)); // 1
+//        System.out.println("a | b: " + (a | b)); // 7
+//        System.out.println("a ^ b: " + (a ^ b)); // 6
+//        System.out.println("~a: " + (~a)); // -6
+//        System.out.println("a << 1: " + (a << 1)); // 10
+//        System.out.println("a >> 1: " + (a >> 1)); // 2
+//        System.out.println("a >>> 1: " + (a >>> 1)); // 2
+
+        // INSTANCEOF OPERATORS
+//        String str = "Hello";
+//        boolean result = str instanceof String;
+//        System.out.println("str is an instance of String: " + result); // true
+
+        // TERNARY OPERATORS
+//        int p = 10, q = 20;
+//        int max = (p > q) ? p : q;
+//        System.out.println("The maximum value is: " + max); // 20
+
+        // UNARY OPERATORS
+//        int a = 10;
+//        System.out.println("++a = " + (++a)); // 11
+//        System.out.println("a++ = " + (a++)); // 11 (then a becomes 12)
+//        System.out.println("--a = " + (--a)); // 11
+//        System.out.println("a-- = " + (a--)); // 11 (then a becomes 10)
+//        boolean b = true;
+//        System.out.println("!b = " + (!b)); // false
+
+    }
+}
+
