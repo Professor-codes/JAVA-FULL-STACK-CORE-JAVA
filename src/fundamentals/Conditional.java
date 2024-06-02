52f158a4536c07392606ddd3c9efa9d5658cc258
sahil <sahiillx02@gmail.com>
2024-06-02 10:28:56 +0530

    conditional

---
+++ src/fundamentals/Conditional.java
@@ -0,0 +1,62 @@
+package fundamentals;
+
+public class Conditional {
+    public static void main(String[] args) {
+
+        // IF STATEMENT
+//        int number = 10;
+//        if (number > 5) {
+//            System.out.println("The number is greater than 5.");
+//        }
+
+        // IF ELSE STATEMENT
+//        int number = 10;
+//        if (number > 15) {
+//            System.out.println("The number is greater than 15.");
+//        } else {
+//            System.out.println("The number is 15 or less.");
+//        }
+
+        // IF ELSE IF LADDER
+//        int number = 10;
+//        if (number > 15) {
+//            System.out.println("The number is greater than 15.");
+//        } else if (number > 10) {
+//            System.out.println("The number is greater than 10 but 15 or less.");
+//        } else if (number > 5) {
+//            System.out.println("The number is greater than 5 but 10 or less.");
+//        } else {
+//            System.out.println("The number is 5 or less.");
+//        }
+
+        // SWITCH STATEMENT
+//        int day = 7;
+//        switch (day) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Invalid day");
+//                break;
+//        }
+
+    }
+}
