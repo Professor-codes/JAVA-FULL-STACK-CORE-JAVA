c6751433ff46204cf46915becc9ab2fc45cff960
sahil <sahiillx02@gmail.com>
2024-06-05 09:39:09 +0530

    constructor:

---
+++ src/oops/ConstructorDemo.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class ConstructorDemo {
+
+    ConstructorDemo() {
+        System.out.println("constructor.");
+    }
+
+    public static void main(String[] args) {
+
+        ConstructorDemo obj1 = new ConstructorDemo();
+        ConstructorDemo obj2 = new ConstructorDemo();
+        ConstructorDemo obj3 = new ConstructorDemo();
+
+    }
+}
