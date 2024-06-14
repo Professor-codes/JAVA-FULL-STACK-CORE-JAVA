95629bbbb67b2b020bfc6c13c029a7c376124ef6
Professor <sahiillx02@gmail.com>
2024-06-14 11:06:38 +0530

    method overloading

--- src/oops/MethodOverloading.java
+++ src/oops/MethodOverloading.java
@@ -1,6 +1,7 @@
 package oops;
 
-class Test {
+public class MethodOverloading {
+
     // METHOD FOR CHAR
     public void print(int i) {
         System.out.println("Integer : " + i);
@@ -15,19 +16,17 @@ class Test {
     public void print(String str) {
         System.out.println("String : " + str);
     }
-}
 
-public class MethodOverloading {
     public static void main(String[] args) {
         int i = 10;
         char ch = 'z';
         String str = "simple text";
 
         // OBJECT
-        Test test = new Test();
-        test.print(i);
-        test.print(ch);
-        test.print(str);
+        MethodOverloading obj = new MethodOverloading();
+        obj.print(i);
+        obj.print(ch);
+        obj.print(str);
 
     }
 }
